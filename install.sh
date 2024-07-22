cab8a9beb67f5a2f4f4fdef1305d51688378b426
Callan Breare <callan.breare@outlook.com>
2024-07-22 10:43:23 +0100

    Updated folder structure and install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "Installing Dependancies"
 sudo pacman -S --needed --no-confirm waybar hyprland qt5ct qt6ct dunst swww plasman-meta gtk3
 
 echo "Copying Config Files"
+cd config
 sudo rsync -aAXvH . /$HOME/.config/
 
 echo "Completed, please reboot"
