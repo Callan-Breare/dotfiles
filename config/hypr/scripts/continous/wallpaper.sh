b36f3e4bc3e1fe136f6c0797dd1b9067d77e97b0
Callan Breare <callan.breare@outlook.com>
2024-08-27 22:27:50 +0100

    general updates

--- config/hypr/scripts/continous/wallpaper.sh
+++ config/hypr/scripts/continous/wallpaper.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 # Directory where your wallpapers are stored
-wallpaper_dir="$HOME/Pictures/WallPaper/"
+wallpaper_dir="/media/Drives/DS2/HOME/Media/Pictures/WallPaper/"
 
 # Get a random wallpaper from the directory
 wallpaper_dp2=$(find "$wallpaper_dir" -type f | shuf -n 1)
 wallpaper_dp3=$(find "$wallpaper_dir" -type f | shuf -n 1)
-
+wallpaper_hmdi1=$(find "$wallpaper_dir" -type f | shuf -n 1)
 # Set the wallpaper using eww. Each monitor has a different wallpaper
 swww img --outputs DP-2 --transition-type none "$wallpaper_dp2"
 swww img --outputs DP-3 --transition-type none "$wallpaper_dp3"
