dccda6f5c300c8638bf1702c1b2296769888cf6d
Callan Breare <callan.breare@outlook.com>
2025-01-07 19:20:37 +0000

    Changes to hyprland configuration and wallpaper managment

--- config/hypr/scripts/continous/wallpaper.sh
+++ config/hypr/scripts/continous/wallpaper.sh
@@ -5,18 +5,14 @@ WALLPAPER_DIR="/media/Drives/DS2/HOME/Media/Pictures/WallPaper/"
 
 # Get random wallpapers for each monitor
 WALLPAPER_DP2=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-WALLPAPER_DP3=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 swww img --outputs DP-2 --transition-type none "$WALLPAPER_DP2"
-swww img --outputs DP-3 --transition-type none "$WALLPAPER_DP3"
 
 # Generate the hyprpaper config file
 cat <<EOL >~/.config/hypr/hyprpaper.conf
 # Hyprpaper Config
 
 wallpaper = DP-2, $WALLPAPER_DP2
-wallpaper = DP-3, $WALLPAPER_DP3
 
 preload = $WALLPAPER_DP2
-preload = $WALLPAPER_DP3
 EOL
