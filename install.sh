fe8df880fa2765c9fb198385586547a5b746698d
Callan Breare <callan.breare@outlook.com>
2025-06-23 15:20:51 +0100

    new install script

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
-echo "Installing Dependancies"
+#!/bin/bash
 
-sudo pacman -S --needed --noconfirm waybar hyprland qt5ct qt6ct dunst swww gtk3
+echo "Backing up existing config files"
 
-echo "Copying Config Files"
+#rename .config directories to old to retain original configuration
+mv $HOME/.config/dunst $HOME/.config/dunst-old
+mv $HOME/.config/hypr $HOME/.config/hypr-old
+mv $HOME/.config/kitty $HOME/.config/kitty-old
+mv $HOME/.config/nvim $HOME/.config/nvim-old
+mv $HOME/.config/waybar $HOME/.config/waybar-old
+mv $HOME/.config/wlogout $HOME/.config/wlogout-old
+mv $HOME/.config/wofi $HOME/.config/wlogout-old
 
-cd config
+#rename zsh files to retain original configuration
+mv $HOME/.zshrc $HOME/.zshrc-old
+mv $HOME/.p10k.zsh $HOME/.p10k.zsh-old
 
-#current_dir = $(pwd)
-#if ["$current_dir" == "$]
-#Need to do a check to make sure we are in the correct dir to copy from
+echo "Backup complete
+Creating symlinks for new dotfiles"
 
-sudo rsync -aAXvH . $HOME/.config/.
+/bin/stow .
 
-echo "Completed, please reboot"
+echo "Install completed, please log out and back in"
