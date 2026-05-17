b01ca6c3c8c650ba1cd4283b433fac09bf5de81e
Callan Breare <callan.breare@outlook.com>
2026-05-17 13:27:42 +0100

    Added hyprland.lua support

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,15 @@
+hl.env("XDG_MENU_PREFIX", "arch-")
+hl.env("HYPRLAND_CONFIG", "lua")
+
+-- Use os.execute for shell commands
+os.execute("bash $HOME/.config/hypr/scripts/startup/start.sh")
+os.execute("kbuildsycoca6 --noincremental")
+os.execute("dbus-update-activation-environment --all")
+
+-- Load all your modular configurations
+require("hyprsource.monitor")
+require("hyprsource.env")
+require("hyprsource.gen")
+require("hyprsource.binds")
+require("hyprsource.windowrules")
+require("hyprsource.layouts")
